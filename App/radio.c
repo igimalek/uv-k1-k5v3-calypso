2d2f6676bf44df186323c1f4f88664beb6836c11
mentanah <153547177+mentanah@users.noreply.github.com>
2026-01-20 20:00:23 +0000

    AF adjustment for steef

--- App/radio.c
+++ App/radio.c
@@ -1103,8 +1103,11 @@ void RADIO_SetModulation(ModulationMode_t modulation)
         BK4819_WriteRegister(0x2a,0x7400);
         BK4819_WriteRegister(0x2b,0);
         BK4819_WriteRegister(0x2f,0x9890);
-        BK4819_WriteRegister(0x54, 0x9009);
-        BK4819_WriteRegister(0x55, 0x31a9);
+        // stock firmware values:
+        //BK4819_WriteRegister(0x54, 0x9009);
+        //BK4819_WriteRegister(0x55, 0x31a9);
+        BK4819_WriteRegister(0x54, 0x8546);
+        BK4819_WriteRegister(0x55, 0x3af0);
     }
     else
     {
@@ -1114,6 +1117,8 @@ void RADIO_SetModulation(ModulationMode_t modulation)
         BK4819_WriteRegister(0x2a,0x7434);
         BK4819_WriteRegister(0x2b,0x300);
         BK4819_WriteRegister(0x2f,0x9990);
+
+
         //BK4819_WriteRegister(0x54, 0x9775);  // calypso
         //BK4819_WriteRegister(0x55, 0x32c6);  // removed for better AM reception
         BK4819_SetFilterBandwidth(BK4819_FILTER_BW_AM, true);
